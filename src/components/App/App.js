e08c05fc775d8848711c39c1002badb3ad9d9c22
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2023-03-21 22:04:53 +0200

    commit_2

---
+++ src/components/App/App.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    console.log(this.state);
+    const { contacts, name } = this.state;
+    return (
+      <div>
+        <section title="Phonebook">
+          Phonebook
+          <form>
+            <label>
+              Name
+              <input
+                type="text"
+                name={name}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </section>
+        <section title="Contacts">
+          Contacts
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
--- src/components/App/App.jsx
+++
@@ -1,8 +0,0 @@
-import { Component } from 'react';
-
-export class App extends Component {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  };
